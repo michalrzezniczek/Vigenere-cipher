22f653143ae91ee3ab2bce0e4ad2b4bb8332bbea
Michal Rzezniczek <michal.rzezniczek@outlook.com>
2014-10-18 23:55:35 +0200

    Add counter of letters frequency

--- VigenereDecrypt.py
+++ VigenereDecrypt.py
@@ -1,1 +1,19 @@
 __author__ = 'Michal Rzezniczek'
+
+cipherTextFileName = 'cipherText.txt'
+decryptedTextFileName = 'decrypted.txt'
+
+frequency = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0,
+             'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
+
+
+def counter(frequencyDictionary, text):
+    for c in text:
+        frequency[c] += 1
+    return frequency
+
+with open(cipherTextFileName, 'r') as cipherTextFile:
+    cipherText = cipherTextFile.read()
+
+
+cipherTextFile.close()
